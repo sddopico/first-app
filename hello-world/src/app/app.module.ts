0679889bc036acc5c9ce5c214b7a3e27a57004bb
Dominic Dopico <serafin.dopico@gmail.com>
2020-04-01 18:35:00 -0700

    generate authors component

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule,
---
+++ hello-world/src/app/authors/authors.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent {
+
+  constructor() { }
+
+
+}
